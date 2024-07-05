a7a97ea36b5deefc359aa95a2b8ea22071285f49
Narendra Rajbhar <narendrarajbhar547.com>
2024-07-05 07:14:01 +0530

    third commit

--- src/ThemeProvider/ThemeProvider.tsx
+++ src/ThemeProvider/ThemeProvider.tsx
@@ -1,1 +1,16 @@
 "use client"
+import React from 'react'
+import { baselightTheme } from "@/utils/theme/DefaultColors";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+const ThemeProviders = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <ThemeProvider theme={baselightTheme}>
+            <CssBaseline />
+            {children}
+        </ThemeProvider>
+    )
+}
+
+export default ThemeProviders
